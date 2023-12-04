ac732afb431edd96bbab8591413d90679f8194bf
belgacem <benamierabelgacem@gmail.com>
2023-12-04 02:51:08 +0100

    feat: Petite refraichement sur le navigateur   dev-tools

---
+++ js/two.js
@@ -0,0 +1,25 @@
+/**
+ * @description      : 
+ * @author           : belgacem
+ * @group            : 
+ * @created          : 01/12/2023 - 16:56:27
+ * 
+ * MODIFICATION LOG
+ * - Version         : 1.0.0
+ * - Date            : 01/12/2023
+ * - Author          : belgacem
+ * - Modification    : 
+**/
+
+/**
+ * refraichement about the web browser 
+ * js engine V8 
+ * execute the code js 
+ * V8 open Source code 
+*/
+
+console.log("hello word");
+
+var t = document.getElementById("test");
+console.log(t);
+t.innerText="update message"
